dcf735356f604da2e10dc968c8f182696ab779b0
mujahed85 <mujahed.mca@gmail.com>
2022-05-28 18:58:11 +0530

    basics of Functions in Python

---
+++ 03.Functions/02.WithFunction.py
@@ -0,0 +1,15 @@
+def myf1(Limit=5):    
+    i=1
+    while i<=Limit:
+        print("AAA")
+        i=i+1
+
+print("First Line")
+myf1()
+print("Second Line")
+myf1(1)
+print("Third Line")
+myf1(2)
+print("Fourth Line")
+myf1()
+print("Fifth Line")
