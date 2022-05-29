44b09987adb32e713f505fd2e73c618e55021a0b
mujahed85 <mujahed.mca@gmail.com>
2022-05-29 19:36:17 +0530

    PyAppDB Added Files

---
+++ 05.PyAppDB/DemoSelect.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+databaseName="egdb"
+
+config = {
+  'user': 'root',
+  'password': '',
+  'host': 'localhost',
+  'database': databaseName,
+  'raise_on_warnings': True
+}
+
+cnx = mysql.connector.connect(**config)
+query="Select id,name from employee"
+cursor=cnx.cursor()
+cursor.execute("USE "+databaseName)
+cursor.execute(query)
+for (id,name) in cursor:
+	print("Your Id is {}, Name is {} ".format(id, name))
+cnx.commit()
+cursor.close()
+cnx.close()
